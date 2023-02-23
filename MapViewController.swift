1ba711e4fd419b32e3051188e18dfd69f4e3f78e
cdup <cdupreez1@gaels.iona.edu>
2023-02-22 20:15:42 -0500

    cara changes

--- MapViewController.swift
+++ MapViewController.swift
@@ -8,50 +8,238 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import CoreLocation
+import AWSSigner
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+var lat: Double = 0.0
+var long: Double = 0.0
+let frameHeight = UIScreen.main.bounds.height
+let frameWidth = UIScreen.main.bounds.width
+var svar: Int = 0;
 
-    var locationManager: CLLocationManager!
-    var currentLocation: CLLocation?
-    var placesClient: GMSPlacesClient!
-    var preciseLocationZoomLevel: Float = 15.0
-    var approximateLocationZoomLevel: Float = 10.0
+
+class MapViewController: UIViewController, CLLocationManagerDelegate //,UITabBarController
+{
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: 43.061827, longitude: -73.744951, zoom: 15.0)
-        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
-        // sets the map type to satellite. ranges 1-5 for different types
-        mapView.mapType = GMSMapViewType(rawValue: 4)!
-        self.view.addSubview(mapView)
-        mapView.settings.scrollGestures = true
-        mapView.settings.zoomGestures = true
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 43.061827, longitude: -73.744951)
-        marker.title = "Saratoga National"
-        marker.snippet = "New York"
-        marker.map = mapView
+    @IBOutlet weak var Settings: UIButton!
+    
+    // ------------------------- START Hole Data Struct --------------------------------------
+    // MARK: - Course
+    struct Course: Codable {
+        let resources: [Resource]
+    }
 
-        // Initialize the location manager.
-        locationManager = CLLocationManager()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.distanceFilter = 50
-        locationManager.startUpdatingLocation()
-        locationManager.delegate = self
+    // MARK: - Resource
+    struct Resource: Codable {
+        let id, number, courseid: Int
+        let rotation: Double
+        let range: Range
+        let dimensions: Dimensions
+        let vectors: [Vector]
+        let flagcoords: Flagcoords
+    }
 
-        placesClient = GMSPlacesClient.shared()
-        
-        // An array to hold the list of likely places.
-        var likelyPlaces: [GMSPlace] = []
-        // The currently selected place.
-        var selectedPlace: GMSPlace?
-        
+    // MARK: - Dimensions
+    struct Dimensions: Codable {
+        let width, height: Int
+    }
+
+    // MARK: - Flagcoords
+    struct Flagcoords: Codable {
+        let lat, long: Double
+    }
+
+    // MARK: - Range
+    struct Range: Codable {
+        let x, y: X
+    }
+
+    // MARK: - X
+    struct X: Codable {
+        let min, max: Double
+    }
+
+    // MARK: - Vector
+    struct Vector: Codable {
+        let type: TypeEnum
+        let lat, long: Double
+    }
+
+    enum TypeEnum: String, Codable {
+        case blue = "Blue"
+        case flag = "Flag"
+        case red = "Red"
+        case white = "White"
+        case yellow = "Yellow"
     }
     
+    // ------------------------- END Hole Data Struct --------------------------------------
+    
+    @IBOutlet var getLocButton: UIButton!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad()
+    {
+        
+        
+        
+        // Fetching the data
+        let credentials = StaticCredential(accessKeyId: "AKIAY4WGH3URC5UYE24U", secretAccessKey: "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")
+        let signer = AWSSigner(credentials: credentials, name: "execute-api", region: "us-east-1")
+        let signedURL = signer.signURL(
+            url: URL(string:"https://api.golfbert.com/v1/courses/13607/holes")!,
+            method: .GET)
+        
+        var request = URLRequest(url: signedURL, timeoutInterval: Double.infinity)
+        request.addValue("xZsYxHwK7L9eiYeSzhBzf8svKqjOwwrUauEOKOKH", forHTTPHeaderField: "x-api-key")
+        
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            //rint(String(data: data, encoding: .utf8)!)
+            let course = try? JSONDecoder().decode(Course.self, from: data)
+            //print("hole one flag coords:\((course?.resources.first)!.flagcoords.lat))")
+            lat = (course?.resources.first)!.flagcoords.lat
+            long = (course?.resources.first)!.flagcoords.long
+            print((course?.resources.first)!.flagcoords.lat)
+            print((course?.resources.first)!.flagcoords.long)
+        }
 
-
+        task.resume()
+        
+        
+        
+        // Making the map
+        let position = CLLocationCoordinate2D(latitude: 40.980249328485, longitude: -73.75963698733936)
+        let marker = GMSMarker(position: position)
+        let camera = GMSCameraPosition(
+            target: position,
+            zoom: 18
+        )
+            let mapView = GMSMapView(frame: self.view.bounds, camera: camera)
+            marker.map = mapView
+            self.view = mapView
+        mapView.mapType = .satellite
+        
+        super.viewDidLoad()
+        createSettingsButton()
+        createFeedbackButton()
+        createEmptyButton()
+        
+     
+       
+    }
+    @objc func SettingsbuttonAction(button: UIButton)
+    {
+        print("button Pressed")
+        svar = 5;
+        
+        performSegue(withIdentifier: "SettingsSegue", sender: self)
+    }
+    
+    @objc func FeedbackbuttonAction(button: UIButton)
+    {
+        print("button Pressed")
+        svar = 10;
+        performSegue(withIdentifier: "FeedbackSegue", sender: self)
+    }
+    
+    @objc func EmptybuttonAction(button: UIButton)
+    {
+        print("button Pressed")
+       // svar = 10;
+        //performSegue(withIdentifier: "FeedbackSegue", sender: self)
+    }
+    
+    func createSettingsButton(){
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 0, y: frameHeight-60, width: 120, height: 60)
+        button.backgroundColor = .white
+        button.setTitle("Settings", for: .normal)
+        button.layer.cornerRadius = 8
+        button.contentEdgeInsets = UIEdgeInsets(
+          top: 10,
+          left: 20,
+          bottom: 10,
+          right: 20
+          )
+    
+        button.addTarget(self, action: #selector(SettingsbuttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+        
+       
+    }
+    
+    func createEmptyButton(){
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: frameWidth/2 - 87, y: frameHeight-60, width: 175, height: 60)
+        button.backgroundColor = .white
+        button.setTitle("Hole 1", for: .normal)
+        button.layer.cornerRadius = 8
+        button.contentEdgeInsets = UIEdgeInsets(
+          top: 10,
+          left: 20,
+          bottom: 10,
+          right: 20
+          )
+    
+        button.addTarget(self, action: #selector(EmptybuttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+        
+       
+    }
+    
+    
+    
+    func createFeedbackButton(){
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: frameWidth-120, y: frameHeight-60, width: 120, height: 60)
+        button.backgroundColor = .white
+        button.setTitle("Feedback", for: .normal)
+        button.layer.cornerRadius = 8
+        button.contentEdgeInsets = UIEdgeInsets(
+          top: 10,
+          left: 20,
+          bottom: 10,
+          right: 20
+          )
+    
+        button.addTarget(self, action: #selector(FeedbackbuttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+        
+       
+    }
+    override func prepare (for segue: UIStoryboardSegue, sender: Any!) {
+      super.prepare(for:segue,sender:sender)
+       if (svar == 5)
+        {
+           let destController = segue.destination as! SettingsViewController
+       }
+       else if (svar == 10)
+        {
+           let destController = segue.destination as! FeedbackViewController
+        }
+    }
+  
+    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        let destController = segue.destination as! SettingsViewController
+        //let newDestController = segue.destination as! FeedbackViewController
+        //destController.receiverStr = self.userNameLabel.text ?? ""
+    }
+    
+   */
+    
+    
+    
+    
+    
+   
 }
+
--- SearchViewController.swift
+++ SearchViewController.swift
@@ -6,27 +6,92 @@
 //
 
 import UIKit
+import MapKit
 
-class SearchViewController: UIViewController {
-
+class SearchViewController : UIViewController, UISearchResultsUpdating
+{
+    
+    
+   
     @IBOutlet weak var userName: UILabel!
     var receiverStr: String = ""
+    let mapView =  MKMapView()
+    let searchVC = UISearchController(searchResultsController: SearchResultsViewController())
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Maps"
+        view.addSubview(mapView)
+        
         userName.text = "Hello " + receiverStr
+        searchVC.searchBar.backgroundColor = .secondarySystemBackground
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        //mapView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.size.width, height: view.frame.size.height - view.safeAreaInsets.top)
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty ,
+              let resultsVC = searchController.searchResultsController as? SearchResultsViewController
+        else{
+            return
+        }
+        resultsVC.delegate = self
+        
+        GooglePlacesManager.shared.findPlaces(query: query){ result in
+            switch result{
+            case .success(let places):
+                DispatchQueue.main.async {
+                    resultsVC.update(with: places)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
+}
+    
+    extension SearchViewController: SearchResultsViewControllerDelegate{
+        func didTapPlace(with coordinates: CLLocationCoordinate2D){
+            searchVC.searchBar.resignFirstResponder()
+            searchVC.dismiss(animated: true, completion: nil)
+            
+            let annotations = mapView.annotations
+            mapView.removeAnnotations(annotations)
+            let pin = MKPointAnnotation()
+            pin.coordinate = coordinates
+            mapView.addAnnotation(pin)
+            mapView.setRegion(
+                MKCoordinateRegion(
+                    center: coordinates,
+                    span: MKCoordinateSpan(
+                latitudeDelta: 0.2,
+                longitudeDelta: 0.2
+                    )),
+                animated: true
+            )
+        }
+    
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+
+
+/*
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destination.
+    // Pass the selected object to the new view controller.
 }
+*/
